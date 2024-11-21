ff769e553775e118706ac7c6cad620f1823181d7
xor <xorman@gmail.com>
2024-11-20 22:04:53 -0500

    [stimulus] more stack pre-config

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -6,4 +6,4 @@ const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
 
-app.register('popover', Popover);
+
