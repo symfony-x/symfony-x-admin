3fc8743055ad8302a3b6aaba8ba4f2fb98eae0d6
xor <xorman@gmail.com>
2024-11-20 22:05:39 -0500

    [symfony] added babdev/pagerfanta-bundle pagerfanta/doctrine-orm-adapter pagerfanta/twig

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     TalesFromADev\Twig\Extra\Tailwind\Bridge\Symfony\Bundle\TalesFromADevTwigExtraTailwindBundle::class => ['all' => true],
     TalesFromADev\FlowbiteBundle\TalesFromADevFlowbiteBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
