3a7be004e420298c917af0cf860c9a9305161d0e
xor <xorman@gmail.com>
2024-12-04 10:43:14 -0500

    [ux] flowbite modal / turbo fixes

---
+++ assets/controllers/modal_controller.js
@@ -0,0 +1,64 @@
+// wip - used for modal views involving stimulus controllers and flowbite
+import { Controller } from '@hotwired/stimulus';
+export default class extends Controller {
+  static targets = ['dialog', 'dynamicContent', 'loadingTemplate'];
+  observer = null;
+  connect() {
+    if (this.hasDynamicContentTarget) {
+      // when the content changes, call this.open()
+      this.observer = new MutationObserver(() => {
+        const shouldOpen = this.dynamicContentTarget.innerHTML.trim().length > 0;
+        if (shouldOpen && !this.dialogTarget.open) {
+          this.open();
+        } else if (!shouldOpen && this.dialogTarget.open) {
+          this.close();
+        }
+      });
+      this.observer.observe(this.dynamicContentTarget, {
+        childList: true,
+        characterData: true,
+        subtree: true
+      });
+    }
+  }
+  disconnect() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+    if (this.dialogTarget.open) {
+      this.close();
+    }
+  }
+  open() {
+    this.dialogTarget.showModal();
+    document.body.classList.add('overflow-hidden', 'blur-sm');
+  }
+  close() {
+    this.dialogTarget.close();
+    document.body.classList.remove('overflow-hidden', 'blur-sm');
+  }
+  clickOutside(event) {
+    if (event.target !== this.dialogTarget) {
+      return;
+    }
+    if (!this.#isClickInElement(event, this.dialogTarget)) {
+      this.dialogTarget.close();
+    }
+  }
+  showLoading() {
+    // do nothing if the dialog is already open
+    if (this.dialogTarget.open) {
+      return;
+    }
+    this.dynamicContentTarget.innerHTML = this.loadingTemplateTarget.innerHTML;
+  }
+  #isClickInElement(event, element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top <= event.clientY &&
+      event.clientY <= rect.top + rect.height &&
+      rect.left <= event.clientX &&
+      event.clientX <= rect.left + rect.width
+    );
+  }
+}
