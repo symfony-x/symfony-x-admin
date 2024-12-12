71cf7d92b6c41d2e3e926b9ab98fb0f12fb125fd
xor <xorman@gmail.com>
2024-12-12 13:57:43 -0500

    [sx] initial setup script - optional

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
+echo "Setting up Symfony-X"
+set -e
+
+echo "Building Docker containers..."
+docker-compose up --build -d
+
+echo "Docker containers built and are running"
