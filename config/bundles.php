8ea98f3a596a0d5204e28d47796adfa456ab167b
xor <xorman@gmail.com>
2024-12-17 15:21:19 -0500

    [sx2] added fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
